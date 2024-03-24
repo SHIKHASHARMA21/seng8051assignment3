e53b019a08f6ee97cf688ed3d5dcb01f8a48bc0d
Shikha Sharma <Ssharma2241@conestogac.on.ca>
2024-03-23 23:50:25 -0400

    truck classAddproject files.

---
+++ Classcar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Car : Vehicle
+{
+    private int seats;
+    private string engineType;
+    private string transmission;
+    private bool convertible;
+
+    public int Seats
+    {
+        get { return seats; }
+        set { seats = value; }
+    }
+
+    public string EngineType
+    {
+        get { return engineType; }
+        set { engineType = value; }
+    }
+
+    public string Transmission
+    {
+        get { return transmission; }
+        set { transmission = value; }
+    }
+
+    public bool Convertible
+    {
+        get { return convertible; }
+        set { convertible = value; }
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Seats: " + Seats);
+        Console.WriteLine("Engine Type: " + EngineType);
+        Console.WriteLine("Transmission: " + Transmission);
+        Console.WriteLine("Convertible: " + Convertible);
+    }
+}
